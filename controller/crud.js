b2602055263d0a1288c0b29d5ec234836da42689
TasmVanum <gaston.cath14@gmail.com>
2023-05-02 13:57:41 -0400

    avances en crud de categoria y de alimentos

--- controller/crud.js
+++ controller/crud.js
@@ -6,9 +6,35 @@ exports.GuardarAlimento = (req, res) =>{
     const precio = req.body.precio;
     const categoria = req.body.categoria;
     const descripcion = req.body.descripcion;
-    console.log(nombre + precio + categoria + descripcion);
 
     conexion.query('INSERT INTO alimento SET ?',{categoria_id_fk: categoria, nombre_alimento:nombre, foto_alimento:"", precio: precio, descripcion:descripcion, estado_alimento_id_fk:1}, (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/');
+        }
+    })
+}
+
+exports.EditarAlimento = (req, res) =>{
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const categoria = req.body.categoria;
+    const descripcion = req.body.descripcion;
+
+    conexion.query('UPDATE alimento SET ?',[{categoria_id_fk: categoria, nombre_alimento:nombre, foto_alimento:"", precio: precio, descripcion:descripcion, estado_alimento_id_fk:1}, id], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/');
+        }
+    })
+}
+
+exports.GuardarCategoria = (req, res) =>{
+    const nombre = req.body.nombre;
+
+    conexion.query('INSERT INTO categoria SET ?',{nombre_categoria : nombre,  foto_categoria: null, estado_categoria_id_fk: 1}, (error, results)=>{
         if(error){
             console.log(error);
         }else{
--- router.js
+++ router.js
@@ -2,9 +2,10 @@ const express = require('express');
 const conexion = require('./database/db');
 const router = express.Router();
 
-router.get('/', (req,res) =>{
 
-    conexion.query('SELECT * FROM alimento', (error, results)=>{
+//MENU PRINCIPAL
+router.get('/', (req,res) =>{
+    conexion.query('SELECT * FROM alimento WHERE estado_alimento_id_fk = 1', (error, results)=>{
         if(error){
             throw error;
         }else{
@@ -20,9 +21,9 @@ router.get('/intranet', (req, res) =>{
     res.render('intranet')
 })
 
-
+//DESPLIEGUE DE INFORMACION DE ALIMENTO
 router.get('/alimentocrud', (req, res) =>{
-    conexion.query('SELECT * FROM alimento', (error, results) =>{
+    conexion.query('SELECT * FROM alimento WHERE estado_alimento_id_fk = 1', (error, results) =>{
         if(error){
             throw error;
         }else{
@@ -31,12 +32,46 @@ router.get('/alimentocrud', (req, res) =>{
     })
 })
 
+//VISTA DE CREACION DE ALIMENTO
 router.get('/crearalimento', (req, res) =>{
     res.render('crearalimento')
 })
 
+//VISTA DE EDICION DE ALIMENTO
+router.get('/editaralimento/:id', (req, res)=>{
+
+    const id = req.params.id;
+    conexion.query('SELECT * FROM alimento WHERE alimento_id = ?', [id], (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('editaralimento', {tipo:results[0]});
+        }
+    })
+})
+
+//DESHABILITAR ALIMENTO
+router.get('/deshabilitarAlimento/:id', (req, res)=>{
+
+    const id = req.params.id;
+    conexion.query('UPDATE alimento SET estado_alimento_id_fk = 2 WHERE alimento_id = ? ', [id], (error)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/alimentocrud')
+        }
+    })
+})
+
+
+router.get('/crearcategoria', (req, res) =>{
+    res.render('crearcategoria')
+})
+
+//DESPLIEGUE DE INFORMACION DE CATEGORIA
+
 router.get('/categoriacrud', (req, res) =>{
-    conexion.query('SELECT * FROM categoria', (error, results) =>{
+    conexion.query('SELECT * FROM categoria WHERE estado_categoria_id_fk = 1', (error, results) =>{
         if(error){
             throw error;
         }else{
@@ -45,9 +80,24 @@ router.get('/categoriacrud', (req, res) =>{
     })
 })
 
+//DESHABILITAR ALIMENTO
+router.get('/deshabilitarCategoria/:id', (req, res)=>{
+
+    const id = req.params.id;
+    conexion.query('UPDATE categoria SET estado_categoria_id_fk = 2 WHERE categoria_id = ? ', [id], (error)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/categoriacrud')
+        }
+    })
+})
+
 
 const crud = require('./controller/crud');
 router.post('/GuardarAlimento', crud.GuardarAlimento);
+router.post('/EditarAlimento', crud.EditarAlimento);
+router.post('/GuardarCategoria', crud.GuardarCategoria);
 
 
 module.exports = router; 
